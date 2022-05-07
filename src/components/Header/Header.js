d72ceac8dde7b0d2cb52fdb894e97956804359c6
wasiushisir <wasiuahmed410@gmail.com>
2022-05-07 20:07:21 +0600

    navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import './Header.css'
 
 const Header = () => {
   const [user, loading, error] = useAuthState(auth);
@@ -10,11 +11,11 @@ const Header = () => {
   
     return (
         <div>
-            <Navbar collapseOnSelect expand="lg" style={{background:"#fff"}} variant="#fff">
+            <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark" sticky="top">
   <Container>
   <Navbar.Brand as ={Link} to="/">Fr Telecom & Inventory Warehouse</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
+  <Navbar.Collapse className='navbar collapse collapse' id="responsive-navbar-nav">
     <Nav className="me-auto">
       <Nav.Link href="#about">About</Nav.Link>
       <Nav.Link href="#feedback">Feedback</Nav.Link>
